323141bdd7dc00bc8766d9936fa6b21a29c1c979
aquilesTrindade <sempedra075@gmail.com>
2024-07-19 03:48:46 -0300

    fix: errors

--- result.js
+++ result.js
@@ -1,8 +1,10 @@
-const container = document.getElementById('container');
+window.onload = function() {
+    const container = document.getElementById('container');
 
-for (let i = 1; i <= 1000; i++) {
-     const p = document.createElement('p');
-     p.className = 'ap';
-     p.textContent = `Eu te amo ${i}`;
-     container.appendChild(p);
-}
+    for (let i = 1; i <= 1000; i++) {
+        const p = document.createElement('p');
+        p.className = 'ap';
+        p.textContent = `Eu te amo ${i}`;
+        container.appendChild(p);
+    }
+};
