8f46e7fce10899fb8bdca06b3a6da52caacc855c
aquilesTrindade <sempedra075@gmail.com>
2024-07-19 03:46:32 -0300

    fix: errors

---
+++ result.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+
+for (let i = 1; i <= 1000; i++) {
+     const p = document.createElement('p');
+     p.className = 'ap';
+     p.textContent = `Eu te amo ${i}`;
+     container.appendChild(p);
+}
